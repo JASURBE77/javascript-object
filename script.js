3c21ecdb3772560a4b3263c8d04f6f4e0e24efcd
JASURBE77 <animesila89@gmail.com>
2025-04-13 11:29:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// let ism = prompt('ismin yoz')
+// let yosh = +prompt('yoshin yoz')
+// let surname = prompt('familiyeni yoz bachha')
+// let gender = prompt('Bormi yoki yoqmi ? bor mosa true yoz yoq bosa false yoz')
+
+// const obj = {
+//     names: ism,
+//     surname: surname,
+//     age: yosh,
+//     gender: null,
+//     cars: {
+//         carName: 'Gentra',
+//         carYear: 2019,
+//         carColor: 'black',
+//         automat: false
+//     },
+//     girl: true,
+//     jonka: {
+//         girlName: 'Umida',
+//         girlYear: 20,
+//         girlBeatfuul: false,
+//         girlFigure: '90-100-90'
+
+//     }
+
+// }
+
+
+// if (gender === 'ha') {
+//     obj.gender = true
+// } else {
+//     obj.gender = false
+// }
+
+
+// obj.jonka.bivshiy = 'yusuf'
+
+// console.log(obj);
+
+// delete obj.jonka.girlFigure
+
+
+
+// let neObj = Object.keys(obj)
+// let newobj = Object.values(obj)
+
+let ism = prompt('ismingizni kiriting')
+
+let idd = +prompt('space idingizni kiriting')
+
+let login = +prompt('space passwordingizni kiriting')
+
+const obj = {
+    names: ism,
+    ID: idd,
+    password: login,
+}
+console.log(obj);
+ alert('space accountingizga xush kelibsiz')
